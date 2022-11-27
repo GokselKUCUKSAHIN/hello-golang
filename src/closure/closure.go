7b34eb9e4ebd9326817f5c1fc7e8f53db7156a3f
gokselkucuksahin <gokselkucuksahin@gmail.com>
2022-11-27 19:55:41 +0300

    Functional Closure, counter example added.

--- src/closure/closure.go
+++ src/closure/closure.go
@@ -8,10 +8,24 @@ func greeter(greetMessage string) func(name string) string {
 	}
 }
 
+func createCounter(initialValue int) func() int {
+	counter := initialValue
+	return func() int {
+		counter++
+		return counter
+	}
+}
+
 func main() {
 	hello := greeter("Hello")
 	hi := greeter("Hi")
 	fmt.Println(hello("Göksel"))
 	fmt.Println(hello("Arda"))
 	fmt.Println(hi("Göksel"))
+
+	myCounter := createCounter(10)
+	fmt.Println(myCounter())
+	fmt.Println(myCounter())
+	fmt.Println(myCounter())
+	fmt.Println(myCounter())
 }
